45cd298c6ad009f4388db863357265bd90f5e37a
victor <vmalvaradop0@gmail.com>
2024-03-19 15:05:40 -0600

    primer commit

---
+++ MySiteTest/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MySiteTest project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MySiteTest.settings")
+
+application = get_asgi_application()
